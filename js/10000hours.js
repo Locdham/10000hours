a01eb1296831059812ac92ec6b714b21e5132587
2DN <hunes11@gmail.com>
2022-04-14 00:19:54 +0900

    3. js 업데이트 시작

---
+++ js/10000hours.js
@@ -0,0 +1,46 @@
+// html에서 가져온 처리해야할 클래스 요소
+const startButton = document.querySelector("start__btn");
+const openButton = document.querySelector("result-btn__go");
+const shareButton = document.querySelector(".result-btn__share");
+const closeButton = document.querySelector("modal__close-btn");
+const result = document.querySelector(".result"); // 디스플레이 on-off
+const modal = document.querySelector(".modal"); // 디스플레이 off-on
+const loading = document.querySelector(".result-loading"); // 디스플레이 off-on
+
+//버튼 클릭 감지 기능
+function calculator() {
+  // 인풋에서 필드,타임 밸류 가져옴
+  const fieldValue = document.querySelector("#field-value");
+  let timeValue = document.querySelector("#time-value");
+  let timeValue_int = Number(timeValue);
+
+  // 리절트에서 필드,타임 리절트 가져옴
+  const fieldResult = document.querySelector(".result__field");
+  const timeResult = document.querySelector(".result__time");
+
+  //입력값 계산 함수
+  if (fieldValue.value == "") {
+    alert("입력되지 않았습니다.");
+    // fieldValue.focus();
+    // return false;
+  } else if (timeValue_int.value == "") {
+    alert("시간이 입력되지 않았습니다.");
+    // timeValue.focus();
+    // return false;
+  } else if (timeValue > 24) {
+    alert("하루는 24시간 입니다. 다시 입력 하세요");
+    return false;
+  }
+
+  result.style.display = "none";
+}
+
+function openModal() {}
+function copyUrl() {}
+function closeModal() {}
+
+//버튼 클릭 감지
+startButton.addEventListener("click", calculator);
+openButton.addEventListener("click", openModal);
+shareButton.addEventListener("click", copyUrl);
+closeButton.addEventListener("click", closeModal);
